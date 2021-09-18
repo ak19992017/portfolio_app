f681e775a6c081231021fd09478a78cc7cf6f5c9
Aditya Kumar <ak19992017@gmail.com>
2021-09-18 10:55:20 +0530

    revision

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,4 @@
-// ignore_for_file: prefer_const_constructors, sized_box_for_whitespace
-
-import 'dart:math';
+// ignore_for_file: prefer_const_constructors, sized_box_for_whitespace, avoid_unnecessary_containers
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -15,8 +13,6 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen>
     with SingleTickerProviderStateMixin {
   late AnimationController controller;
-  late Animation scaleAnimation;
-  late Animation rotateAnimation;
   late Animation<Offset> translateAnimation;
 
   @override
@@ -24,11 +20,6 @@ class _HomeScreenState extends State<HomeScreen>
     super.initState();
     controller =
         AnimationController(vsync: this, duration: Duration(seconds: 3));
-
-    scaleAnimation = Tween<double>(begin: 0, end: 1).animate(
-        CurvedAnimation(parent: controller, curve: Curves.fastOutSlowIn));
-    rotateAnimation = Tween<double>(begin: 0, end: pi)
-        .animate(CurvedAnimation(parent: controller, curve: Curves.bounceOut));
     translateAnimation = Tween<Offset>(
             begin: Offset.zero, end: Offset(1.5, 0.0))
         .animate(CurvedAnimation(parent: controller, curve: Curves.elasticIn));
@@ -37,11 +28,12 @@ class _HomeScreenState extends State<HomeScreen>
       setState(() {});
     });
 
-    controller.repeat(reverse: true);
+    controller.reverse(from: 1);
   }
 
   @override
   void dispose() {
+    controller.dispose();
     super.dispose();
   }
 
@@ -52,55 +44,65 @@ class _HomeScreenState extends State<HomeScreen>
     return Builder(builder: (context) {
       return Scaffold(
         extendBodyBehindAppBar: true,
-        appBar: AppBar(
-          title: Text(
-            "portfolio",
-            style: TextStyle(
-              fontFamily: "Comfortaa",
-              color: Colors.black,
-              fontSize: 30,
-            ),
-          ),
-          iconTheme: IconThemeData(color: Colors.white),
-          elevation: 0,
-          toolbarHeight: screenHeight / 6,
-          backgroundColor: Colors.transparent,
+        appBar: buildAppBar(),
+        body: buildSection(screenWidth, screenHeight),
+        endDrawer: Container(
+          width: screenWidth < 650 ? screenWidth : screenWidth * 0.5,
+          child: Drawer(),
         ),
-        body: Container(
-          width: screenWidth,
-          height: screenHeight,
-          child: Row(
-            children: [
-              Expanded(
-                flex: 7,
-                child: Container(
-                  color: Colors.white70,
-                  child: SlideTransition(
-                    position: translateAnimation,
-                    child: Text(
-                      """Hello, my\nname’s Aditya.\nI’m a Flutter\nDeveloper.""",
-                      style: TextStyle(
-                        fontSize: screenWidth / 16,
-                        fontWeight: FontWeight.bold,
-                        height: 1.0,
-                        letterSpacing: 0.01,
-                      ),
-                    ),
-                  ),
-                  alignment: Alignment.center,
-                ),
-              ),
-              Expanded(
-                flex: 5,
-                child: Container(
-                  color: Color(0XFF9747FF),
-                ),
-              ),
-            ],
-          ),
-        ),
-        endDrawer: Drawer(),
       );
     });
+  }
+
+  Container buildSection(double screenWidth, double screenHeight) {
+    return Container(
+      width: screenWidth,
+      height: screenHeight,
+      child: Row(
+        children: [
+          Expanded(
+            flex: 7,
+            child: Container(
+              color: Colors.white70,
+              child: SlideTransition(
+                position: translateAnimation,
+                child: Text(
+                  """Hello, my\nname’s Aditya.\nI’m a Flutter\nDeveloper.""",
+                  style: TextStyle(
+                    fontSize: screenWidth / 18,
+                    fontWeight: FontWeight.bold,
+                    height: 1.0,
+                    letterSpacing: 0.01,
+                  ),
+                ),
+              ),
+              alignment: Alignment.center,
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: Container(
+              color: Color(0XFF9747FF),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      title: Text(
+        "portfolio",
+        style: TextStyle(
+          fontFamily: "Comfortaa",
+          color: Colors.black,
+          fontSize: 30,
+        ),
+      ),
+      iconTheme: IconThemeData(color: Colors.white, size: 50),
+      elevation: 0,
+      backgroundColor: Colors.transparent,
+    );
   }
 }
