5b6ce04253f5861b6deffac693fb827f9d59d764
Aditya Kumar <ak19992017@gmail.com>
2021-09-23 21:03:10 +0530

    Hide appbar and fab on scroll down and show on scroll up

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:portfolio_app/widgets/custom_button.dart';
 import 'package:portfolio_app/widgets/custom_drawer.dart';
 import 'package:portfolio_app/widgets/on_hover_button.dart';
@@ -18,6 +19,7 @@ class _HomeScreenState extends State<HomeScreen>
     with SingleTickerProviderStateMixin {
   late AnimationController controller;
   late Animation<Offset> translateAnimation;
+  bool isFabVisible = true;
 
   @override
   void initState() {
@@ -47,76 +49,94 @@ class _HomeScreenState extends State<HomeScreen>
     // double screenHeight = MediaQuery.of(context).size.height;
     return Builder(
       builder: (context) {
-        return SafeArea(
-          child: Scaffold(
-            extendBodyBehindAppBar: true,
-            appBar: AppBar(
-              title: Text(
-                "portfolio",
-                style: TextStyle(fontFamily: "Comfortaa"),
+        return Scaffold(
+          extendBodyBehindAppBar: true,
+          body: NestedScrollView(
+            floatHeaderSlivers: true,
+            headerSliverBuilder: (context, innerBoxIsScrolled) => [
+              SliverAppBar(
+                floating: true,
+                snap: true,
+                title: Text(
+                  "portfolio",
+                  style: TextStyle(fontFamily: "Comfortaa"),
+                ),
+                elevation: 0,
+                centerTitle: true,
+                // backgroundColor: Colors.transparent,
               ),
-              elevation: 0,
-              centerTitle: true,
-              // backgroundColor: Colors.transparent,
-            ),
-            body: SingleChildScrollView(
-              child: Column(
-                children: [
-                  Section(
-                    color: Colors.white,
-                    child: Row(
-                      children: [
-                        Expanded(
-                          flex: 7,
-                          child: Container(
-                            color: Colors.white70,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                SlideTransition(
-                                  position: translateAnimation,
-                                  child: Text(
-                                    """Hello, my\nname’s Aditya.\nI’m a Flutter\nDeveloper.""",
-                                    style: TextStyle(
-                                      fontSize: screenWidth / 18,
-                                      fontWeight: FontWeight.bold,
-                                      height: 1.0,
-                                      letterSpacing: 0.01,
+            ],
+            body: NotificationListener<UserScrollNotification>(
+              onNotification: (notification) {
+                if (notification.direction == ScrollDirection.forward) {
+                  if (!isFabVisible) setState(() => isFabVisible = true);
+                } else if (notification.direction == ScrollDirection.reverse) {
+                  if (isFabVisible) setState(() => isFabVisible = false);
+                }
+                return true;
+              },
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Section(
+                      color: Colors.white,
+                      child: Row(
+                        children: [
+                          Expanded(
+                            flex: 7,
+                            child: Container(
+                              color: Colors.white70,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  SlideTransition(
+                                    position: translateAnimation,
+                                    child: Text(
+                                      """Hello, my\nname’s Aditya.\nI’m a Flutter\nDeveloper.""",
+                                      style: TextStyle(
+                                        fontSize: screenWidth / 18,
+                                        fontWeight: FontWeight.bold,
+                                        height: 1.0,
+                                        letterSpacing: 0.01,
+                                      ),
                                     ),
                                   ),
-                                ),
-                                SizedBox(height: 40),
-                                OnHoverButton(
-                                  child: CustomButton(
-                                    text: "Discover",
-                                    onPressed: () {},
-                                    color: Colors.redAccent,
+                                  SizedBox(height: 40),
+                                  OnHoverButton(
+                                    child: CustomButton(
+                                      text: "Discover",
+                                      onPressed: () {},
+                                      color: Colors.redAccent,
+                                    ),
                                   ),
-                                ),
-                              ],
+                                ],
+                              ),
+                              alignment: Alignment.center,
                             ),
-                            alignment: Alignment.center,
                           ),
-                        ),
-                        if (screenWidth > 650)
-                          Expanded(
-                            flex: 5,
-                            child: Container(
-                              color: Color(0XFF9747FF),
-                            ),
-                          )
-                      ],
+                          if (screenWidth > 650)
+                            Expanded(
+                              flex: 5,
+                              child: Container(
+                                color: Color(0XFF9747FF),
+                              ),
+                            )
+                        ],
+                      ),
                     ),
-                  ),
-                  Section(color: Colors.green, child: Container()),
-                  Section(color: Colors.amber, child: Container()),
-                ],
+                    Section(color: Colors.green, child: Container()),
+                    Section(color: Colors.amber, child: Container()),
+                    Section(color: Colors.redAccent, child: Container()),
+                    Section(color: Colors.deepPurpleAccent, child: Container()),
+                  ],
+                ),
               ),
             ),
-            drawer: CustomDrawer(context: context, screenWidth: screenWidth),
-            floatingActionButton:
-                FloatingActionButton(onPressed: () {}, child: Icon(Icons.add)),
           ),
+          drawer: CustomDrawer(context: context, screenWidth: screenWidth),
+          floatingActionButton: isFabVisible
+              ? FloatingActionButton(onPressed: () {}, child: Icon(Icons.add))
+              : null,
         );
       },
     );
